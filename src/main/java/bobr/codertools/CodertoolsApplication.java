c63e30b3183c6d99ef943c7cf42bd566448e6676
bobr <bobrus91@gmail.com>
2024-03-12 02:21:31 +0300

    Initial commit

---
+++ src/main/java/bobr/codertools/CodertoolsApplication.java
@@ -0,0 +1,13 @@
+package bobr.codertools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodertoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodertoolsApplication.class, args);
+    }
+
+}
